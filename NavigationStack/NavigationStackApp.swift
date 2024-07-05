8f58fe9c55c44eb5645217bc6233b1b216d35efe
Gokul <gokulplkl@gmail.com>
2024-07-05 15:54:30 -0400

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Gokul P on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
